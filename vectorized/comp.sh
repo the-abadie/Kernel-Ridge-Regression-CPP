568ceebf87bfe930e85cbdd71b3f8a9e5d1d4411
the-abadie <davidevan3605@gmail.com>
2025-04-17 14:19:57 -0400

    Improved vectorization. About ~10% increase on fitting and evaluating

--- vectorized/comp.sh
+++ vectorized/comp.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+g++ -I lib/ test.cpp -o test -O3
+
 # split.py CL arguments:
 # arg1: N_train (int)
 # arg2: Extend Data? Yes = 1, No = 0
+# python ./data/split.py 1000 0
 
-python ./data/split.py 1000 0
-g++ -I lib/ hpc/krr.cpp -o krr_vec -O3
-
+./test
--- vectorized/hpc/comp.sh
+++
@@ -1,2 +0,0 @@
-g++ -I lib/ krr.cpp -o krr -O3
-./krr
